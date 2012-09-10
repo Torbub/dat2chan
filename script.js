767f1aef6e1225bac7488d31d0b638480004ff45
Emil Nauerby <emil@nauerby.com>
2012-09-10 19:13:16 +0200

    Added a bit of JavaScript.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('#hide-watcher').toggle(
         function() {
+<<<<<<< HEAD
             $('#hide-watcher > a').blur();
             $('#watchlist, #searchlist').hide(250);
             if (!$('#right-menu').attr('old-height')) {
@@ -17,6 +18,18 @@ $(document).ready(function() {
             $('#watchlist, #searchlist').show(250);
             var h = $('#right-menu').attr('old-height');
             $('#hide-watcher > a').text('Hide');
+=======
+            $('#watchlist, #searchlist').hide(250);
+            var h = $('#right-menu').height();
+            $('#right-menu').attr('old-height', h);
+            $('#right-menu').animate({
+                width: '35px',
+                height: '20px'
+            }, 250);
+        }, function() {
+            $('#watchlist, #searchlist').show(250);
+            var h = $('#right-menu').attr('old-height');
+>>>>>>> Added a bit of JavaScript.
             $('#right-menu').animate({
                 width: "25%",
                 height: h
